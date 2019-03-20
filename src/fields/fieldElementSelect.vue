f9a7640f70b9b7322fe0b1bd2183cfd6c4a26d6e
Egor Zotov <me@egor.work>
2019-03-20 09:00:49 +0300

    add Select

--- src/fields/fieldElementSelect.vue
+++ src/fields/fieldElementSelect.vue
@@ -1,32 +1,68 @@
 <template>
-  <el-select
-    v-model="value"
-    placeholder="Select"
-    :disabled="disabled"
-    :name="schema.inputName"
-    :class="schema.fieldClasses"
-    :multiple="schema.multiple"
-    :filterable="schema.filterable"
-  >
-    <el-option
-      v-for="item in items"
-      :key="getItemValue(item)"
-      :label="getItemName(item)"
-      :value="getItemValue(item)"
-    />
-  </el-select>
+  <el-form-item :label="schema.elementLabel ? schema.elementLabel : ''">
+    <el-select
+      v-model="value"
+      :placeholder="schema.placeholder"
+      :disabled="schema.disabled"
+      :name="schema.inputName"
+      :id="schema.id"
+      :value-key="schema.valueKey"
+      :multiple="schema.multiple"
+      :filterable="schema.filterable"
+      :clearable="schema.clearable"
+      :collapse-tags="schema.collapseTags"
+      :multiple-limit="schema.multipleLimit"
+      :autocomplete="schema.autocomplete"
+      :allow-create="schema.allowCreate"
+      :filter-method="schema.filterMethod"
+      :remote="schema.remote"
+      :remote-method="schema.remoteMethod"
+      :loading-text="schema.loadingText"
+      :no-match-text="schema.noMatchText"
+      :no-data-text="schema.noDataText"
+      :popper-class="schema.popperClass"
+      :reserve-keyword="schema.reserveKeyword"
+      :default-first-option="schema.defaultFirstOption"
+      :popper-append-to-body="schema.popperAppendToBody"
+      :automatic-dropdown="schema.automaticDropdown"
+      :size="schema.size"
+    >
+      <el-option
+        v-for="item in items"
+        :key="getItemValue(item)"
+        :label="getItemName(item)"
+        :value="getItemValue(item)"
+      />
+    </el-select>
+  </el-form-item>
 </template>
 
 <script>
+//todo: сделать группировку, протестировать (смотри group)
 import { isObject, isNil, find } from "lodash";
 import { abstractField } from "vue-form-generator";
-
 export default {
+  name: "fieldElementSelect",
   mixins: [abstractField],
+  created() {
+    if (!this.model) {
+      return;
+    }
+    if (typeof this.value === "object" && this.value === null) {
+      if (
+        this.schema.hasOwnProperty("model") &&
+        this.model.hasOwnProperty(this.schema.model)
+      ) {
+        this.value = this.model[this.schema.model];
+      } else if (this.schema.hasOwnProperty("default")) {
+        this.value = this.schema.default;
+      }
+    }
+  },
   computed: {
-    selectOptions() {
-      return this.schema.selectOptions || {};
-    },
+    // selectOptions() {
+    //   return this.schema.selectOptions || {};
+    // },
 
     items() {
       const values = this.schema.values;
@@ -39,6 +75,9 @@ export default {
 
   methods: {
     formatValueToField(value) {
+      if (this.schema.multiple && !Array.isArray(value)) {
+        return [value];
+      }
       if (isNil(value)) {
         return null;
       }
@@ -106,12 +145,12 @@ export default {
 
     getItemValue(item) {
       if (isObject(item)) {
-        if (
-          typeof this.schema.selectOptions !== "undefined" &&
-          typeof this.schema.selectOptions.value !== "undefined"
-        ) {
-          return item[this.schema.selectOptions.value];
-        }
+        // if (
+        //   typeof this.schema.selectOptions !== "undefined" &&
+        //   typeof this.schema.selectOptions.value !== "undefined"
+        // ) {
+        //   return item[this.schema.selectOptions.value];
+        // }
         // Use 'id' instead of 'value' cause of backward compatibility
         if (typeof item.id !== "undefined") {
           return item.id;
@@ -124,12 +163,12 @@ export default {
 
     getItemName(item) {
       if (isObject(item)) {
-        if (
-          typeof this.schema.selectOptions !== "undefined" &&
-          typeof this.schema.selectOptions.name !== "undefined"
-        ) {
-          return item[this.schema.selectOptions.name];
-        }
+        // if (
+        //   typeof this.schema.selectOptions !== "undefined" &&
+        //   typeof this.schema.selectOptions.name !== "undefined"
+        // ) {
+        //   return item[this.schema.selectOptions.name];
+        // }
         if (typeof item.name !== "undefined") {
           return item.name;
         }
