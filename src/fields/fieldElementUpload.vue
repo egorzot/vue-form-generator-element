21d3ed32edf1c6f2d599ca549e7b9037dea1bcd1
Egor Zotov <me@egor.work>
2019-03-25 07:08:03 +0300

    add upload

--- src/fields/fieldElementUpload.vue
+++ src/fields/fieldElementUpload.vue
@@ -1,69 +1,52 @@
 <template>
-  <el-upload
-    action="https://jsonplaceholder.typicode.com/posts/"
-    :on-preview="handlePreview"
-    :on-remove="handleRemove"
-    :before-remove="beforeRemove"
-    :multiple="schema.multiple"
-    :limit="schema.limit"
-    :on-exceed="handleExceed"
-    :file-list="schema.fileList"
-    :name="schema.inputName"
-    :readonly="schema.readonly"
-    :required="schema.required"
-    :accept="schema.accept"
-    :drag="schema.drag"
-    :disabled="disabled"
-  >
-    <el-button v-if="!schema.drag" size="small" type="primary">
-      {{ schema.placeholder || "Click to upload" }}
-    </el-button>
+  <el-form-item :label="schema.elementLabel ? schema.elementLabel : ''">
+    <el-upload
+      :action="schema.action"
+      :on-preview="schema.onPreview"
+      :on-remove="schema.onRemove"
+      :on-exceed="schema.onExceed"
+      :on-error="schema.onError"
+      :on-success="schema.onSuccess"
+      :on-progress="schema.onProgress"
+      :on-change="schema.onChange"
+      :before-remove="schema.beforeRemove"
+      :before-upload="schema.beforeUpload"
+      :multiple="schema.multiple"
+      :limit="schema.limit"
+      :file-list="schema.fileList"
+      :name="schema.inputName"
+      :accept="schema.accept"
+      :drag="schema.drag"
+      :disabled="schema.disabled"
+      :auto-upload="schema.autoUpload"
+      :http-request="schema.httpRequest"
+      :thumbnail-mode="schema.thumbnailMode"
+      :show-file-list="schema.showFileList"
+      :with-credentials="schema.withCredentials"
+      :data="schema.data"
+      :headers="schema.headers"
+      :list-type="schema.listType"
+    >
+      <el-button v-if="!schema.drag" size="small" type="primary">
+        {{ schema.placeholder || "Click to upload" }}
+      </el-button>
 
-    <i v-if="schema.drag" class="el-icon-upload" />
-    <div v-if="schema.drag" class="el-upload__text">
-      {{ schema.placeholder || `Drop file here or click to upload` }}
-    </div>
+      <i v-if="schema.drag" class="el-icon-upload" />
+      <div v-if="schema.drag" class="el-upload__text">
+        {{ schema.placeholder || `Drop file here or click to upload` }}
+      </div>
 
-    <div v-if="schema.tip" slot="tip" class="el-upload__tip">
-      {{ schema.tip }}
-    </div>
-  </el-upload>
+      <div v-if="schema.tip" slot="tip" class="el-upload__tip">
+        {{ schema.tip }}
+      </div>
+    </el-upload>
+  </el-form-item>
 </template>
 
 <script>
 import { abstractField } from "vue-form-generator";
-import { isFunction } from "lodash";
 
 export default {
-  mixins: [abstractField],
-  data() {
-    return {
-      fileList: null
-    };
-  },
-  methods: {
-    onChange() {
-      if (isFunction(this.schema.onChanged)) {
-        // Schema has defined onChange method.
-        this.schema.onChanged.call(this, this.model, this.schema, event, this);
-      }
-    },
-    handleRemove(file, fileList) {
-      console.log(file, fileList);
-    },
-    handlePreview(file) {
-      console.log(file);
-    },
-    handleExceed(files, fileList) {
-      this.$message.warning(
-        `The limit is ${this.schema.limit}, you selected ${
-          files.length
-        } files this time, add up to ${files.length + fileList.length} totally`
-      );
-    },
-    beforeRemove(file, fileList) {
-      return this.$confirm(`Удалить ${file.name}？`);
-    }
-  }
+  mixins: [abstractField]
 };
 </script>
