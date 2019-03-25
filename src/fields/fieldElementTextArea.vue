53cee116ecafe47a928dca0c3b28d8bfaad3293b
Egor Zotov <me@egor.work>
2019-03-25 06:09:41 +0300

    add textarea

--- src/fields/fieldElementTextArea.vue
+++ src/fields/fieldElementTextArea.vue
@@ -1,20 +1,27 @@
 <template>
-  <el-input
-    v-model="value"
-    type="textarea"
-    :placeholder="schema.placeholder"
-    :class="schema.fieldClasses"
-    :disabled="disabled"
-    :name="schema.inputName"
-    :readonly="schema.readonly"
-    :autosize="{ minRows: schema.rows || 2 }"
-  />
+  <el-form-item :label="schema.elementLabel ? schema.elementLabel : ''">
+    <el-input
+      v-model="value"
+      :value="value"
+      type="textarea"
+      :placeholder="schema.placeholder"
+      :disabled="schema.disabled"
+      :name="schema.inputName"
+      :id="schema.id"
+      :readonly="schema.readonly"
+      :autosize="schema.autosize"
+      :rows="schema.rows"
+      :alt="schema.alt"
+    />
+  </el-form-item>
 </template>
 
 <script>
 import { abstractField } from "vue-form-generator";
+import defaultValueSetter from "../mixins/defaultValueSetter";
 
 export default {
-  mixins: [abstractField]
+  name: "fieldElementTextArea",
+  mixins: [abstractField, defaultValueSetter]
 };
 </script>
